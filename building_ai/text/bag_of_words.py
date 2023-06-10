ea14b162c0923328021cfb8883bc20a8663c1628
guiyo-rodriguez <guiller_2912@yahoo.com.ar>
2023-06-10 18:58:51 -0300

    Initial version

---
+++ building_ai/text/bag_of_words.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+data = [[1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 0, 1, 3, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1]]
+
+def find_nearest_pair(data):
+    N = len(data)
+    dist = np.empty((N, N), dtype=float)
+    print(np.unravel_index(np.argmin(dist), dist.shape))
+
+find_nearest_pair(data)
