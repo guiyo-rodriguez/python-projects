d8afbf30c0e4bf1a11c6139277183e6ee5c1eace
guiyo-rodriguez <guiller_2912@yahoo.com.ar>
2023-06-17 22:52:03 -0300

    Initial version.

--- building_ai/text/bag_of_words.py
+++ building_ai/text/bag_of_words.py
@@ -6,9 +6,24 @@ data = [[1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
         [1, 1, 1, 0, 1, 3, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1]]
 
+def dist_manhattan(a, b):
+    return np.sum(np.abs(np.array(a) - np.array(b)))
+
 def find_nearest_pair(data):
     N = len(data)
+    #print("N: ", N)
     dist = np.empty((N, N), dtype=float)
+    dist.fill(np.inf)
+    
+    for x in range(N):
+        for y in range(N):
+            if x == y:
+                continue
+            dist[x, y] = dist_manhattan(data[x], data[y])
+            #print("x: ", x, "y: ", y, "dist: ", dist[x, y])
+
+    #print("dist: \n", dist)
+    #print("np.argmin(dist): ", np.argmin(dist))
     print(np.unravel_index(np.argmin(dist), dist.shape))
 
 find_nearest_pair(data)
